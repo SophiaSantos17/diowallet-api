f68c99cc7e74f874924c6237c49300530e2cad6d
SophiaSantos17 <santosvsophia17@gmail.com>
2024-01-17 15:12:15 -0300

    feat: cors added

--- src/server.js
+++ src/server.js
@@ -2,12 +2,13 @@ import express, { json } from "express";
 import authRouter from "./routes/authRoutes.js";
 import transactionRouter from "./routes/transactionRoutes.js";
 import { connectDB } from "./config/database.js";
-
+import cors from "cors";
 
 const app = express();
 
 connectDB();
 app.use(json());
+app.use(cors());
 app.use(authRouter);
 app.use(transactionRouter);
 
